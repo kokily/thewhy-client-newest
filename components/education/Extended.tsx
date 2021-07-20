c95052ef2bf1e18cf4c917e0265898fad50f40dd
kokily <kokily@kokilyui-MacBookPro.local>
2021-07-20 18:29:57 +0900

    Extended Page

---
+++ components/education/Extended.tsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import Communication from './common/Communication';
+
+interface Props {}
+
+const Extended: React.FC<Props> = () => {
+  const data = [
+    {
+      img: 'target',
+      list: [
+        '소통 및 조직 활성화에 이슈가 있는 조직',
+        '개인의 행동성향 이해가 필요한 조직',
+        '팀진단을 통해 팀을 효과적으로 운영하고 싶은 관리자',
+        '조직에 맞는 인재상을 찾고 싶은 관리자',
+        '개인의 이해를 통한 조직의 소통을 활성화하고 싶은 관리자',
+      ],
+    },
+    {
+      img: 'character',
+      list: ['DISC 진단을 통한 개인별, 팀별 리포트 제공'],
+    },
+    {
+      img: 'program',
+      list: [
+        '개인 성향 진단',
+        '팀 성향 진단',
+        '팀&조직문화 진단',
+        'DISC for 서비스 역량',
+        'DISC for 세일즈 역량',
+        'DISC for 리더의 역량',
+      ],
+      end: true,
+    },
+  ];
+
+  return (
+    <Communication
+      img="edu04.png"
+      title="Extended-DISC 커뮤니케이션"
+      body={
+        '확장된 DISC 진단을 통해 조직 구성원 개인의 성향뿐만 아니라 조직의 성향을 분석합니다.\n개인, 팀, 조직의 소통 능력 활성화 뿐만 아니라 채용, 배치, 교육훈련을 위한 기초 데이터를 제공합니다.\n*Extended-DISC가 궁금하신 분, DISC의 성향 분석을 통한 교육 프로그램을 염두에 두고 계시다면 문의하세요.'
+      }
+      data={data}
+    />
+  );
+};
+
+export default Extended;
---
+++ pages/education/extended.tsx
@@ -0,0 +1,28 @@
+import { NextPage } from 'next';
+import Head from 'next/head';
+import PageTemplate from '../../components/common/PageTemplate';
+import Extended from '../../components/education/Extended';
+
+const ExtendedPage: NextPage = () => {
+  return (
+    <>
+      <Head>
+        <title>확장된 DISC 커뮤니케이션 - 더와이컨설팅</title>
+        <link rel="canonical" href="https://thewhy.kr/education/extended" />
+        <meta
+          name="keywords"
+          content="이디스크,디스크교육,고객성향,고객응대방법,소통,조직활성화,DISC"
+        />
+        <meta
+          name="description"
+          content="커뮤니케이션 교육, 확장된 DISC 진단을 통한 조직 성향 분석"
+        />
+      </Head>
+      <PageTemplate>
+        <Extended />
+      </PageTemplate>
+    </>
+  );
+};
+
+export default ExtendedPage;
